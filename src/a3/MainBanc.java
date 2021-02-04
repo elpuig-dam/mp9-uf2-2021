ce25c7c4e8ef9ffe3fe89c5971fd121560bdce35
jherna <jherna76@elpuig.xeill.net>
2021-02-04 19:28:22 +0100

    add exercici Divisio per restes

---
+++ src/a3/MainBanc.java
@@ -0,0 +1,20 @@
+package a3;
+
+public class MainBanc {
+
+    public static void main(String[] args) throws InterruptedException {
+        CompteEstalvi compteEstalvi = new CompteEstalvi();
+        Thread op1 = null;
+        Thread op2 = null;
+        for(int i= 0; i < 1000; i++) {
+            op1 = new Thread(() -> compteEstalvi.ingres(100));
+            op2 = new Thread(() -> compteEstalvi.treu(50));
+            op1.start();
+            op2.start();
+        }
+        op1.join();
+        op2.join();
+
+        System.out.println("Total saldo: " + compteEstalvi.getSaldo());
+    }
+}
